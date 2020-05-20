7d42e196878933a4c8c91c3166b1097cd178dca8
vandersoncmd-dev <55470760+vandersoncmd-dev@users.noreply.github.com>
2020-05-20 15:30:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ function addBola(){
     var bola = document.createElement("div");
     bola.setAttribute("class","bola");
     var cor = Math.floor(Math.random()*899)+100;
-    var p1 = Math.floor(Math.random() * 1500);
-    var p2 = Math.floor(Math.random() * 780);
+    var p1 = Math.floor(Math.random() * 320);
+    var p2 = Math.floor(Math.random() * 600);
     bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;background-color:#"+cor+";");
     bola.setAttribute("onclick", "estourar(this)");
 
     document.body.appendChild(bola); 
-    cont = cont + 1;
-    console.log(cont);
 }
 function estourar(elemento) {
+	cont = cont + 1;
+	var audio = document.querySelector('audio');
+	audio.play();
+	document.getElementById('pts').innerHTML = cont;
     document.body.removeChild(elemento);
+	if(cont == 10){
+		window.location.href="#";
+	}
+	
 
 }
 function iniciar(){
